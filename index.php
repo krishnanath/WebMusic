3f65e06a671aeb22b0de50a1d00944df594034c1
krishnanath <malykrishna@gmail.com>
2019-01-25 14:41:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 Hello
 Anjitha 
 I love You
+
+
+
+<?php
+if(isset($_POST['loginButton'])) {
+//echo "login Button Pressed";
+}
+if(isset($_POST['registerButton'])) {
+ echo "Register Button Pressed";
+    
+    }
+?>
